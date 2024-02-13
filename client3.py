8aeb4c977013cf63b0dab7db73d2207362e6d39e
Joshua Casuga <jomatthew8@gmail.com>
2024-02-12 18:25:11 -0800

    commit 1

--- client3.py
+++ client3.py
@@ -42,7 +42,7 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if(price_b == 0): # avoid divide by zero error
+    if(price_b == 0): # avoid divide by zero exception
         return
     return price_a / price_b
 
